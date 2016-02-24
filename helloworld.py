4288ba2e3c041d81b0a8228acc8f9f5a9d20244a
Nathan Spencer <nspencer1@mail.valenciacollege.edu>
2016-02-24 17:28:47 -0500

    Asks the user to select a language and says Hello! in the language
    Hello world Mini project

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,32 @@
-# replace the contents of this comment with your full name
+# Nathan Ezra Spencer
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+import sys # including sys to exit the program later
 
-# make sure that your code contains comments explaining your logic!
+print ('Hello!') # greets the user in English
+print ('Please select a language:')
+print ('English, German, or Russian') # my 3 favorite languages
+
+language = input() #using input() to allow the user to choose a language
+if language == 'English': # if the variable language is set to any of the strings, it prints the correct response
+    print ('Hello!')
+if language == 'German':
+    print ('Hallo!')
+if language == 'Russian':
+    print('Здравствуйтe!')
+    
+print ('goodbye!')# saying goodbye
+print ('type "exit" to leave program')# allowing the user to exit the program using sys.exit()
+ext = input()
+if ext == 'exit':
+    sys.exit()
+
+    # please provide feedback
+
+    
+
+
+
+
+
+
 
